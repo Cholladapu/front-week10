0270781a724dd6f30196175d1209e1c2fdf8679e
chollada punseenam <chollada.pu@ku.th>
2024-02-26 21:15:45 +0700

    edit

--- src/front/Product.js
+++ src/front/Product.js
@@ -1,20 +1,27 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-export default function Product(){
-    const [product,setProduct]=useState([])
-    useEffect(()=>{
-        console.log("request to api")
-        axios.get("http://127.0.0.1:5000/products")
-        .then(response=>setProduct(response.data))
-        .catch(error => {
-            console.error('Error fetching data:', error);
-          })
-    },[])
-    const productList=product.map(p=><li key={p.id}>{p.id} 
-    {p.name} <img src={p.img}/> {p.price}</li>)
-    return (<>
-        <ul>
-            {productList}
-        </ul>
-    </>)
+
+export default function Product() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/products')
+            .then(response => setProducts(response.data.products))
+            .catch(error => console.error('Error fetching products:', error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Products </h1>
+            <ul>
+                {products.map(product => (
+                    <li key={product._id}>
+                        <img src={product.img} alt={product.name} />
+                        <div>{product.name}</div>
+                        <div>Price: {product.price}</div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
