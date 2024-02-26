4b3ef9d007fd21725927cd8cbfb3b54a3573839c
chollada punseenam <chollada.pu@ku.th>
2024-02-26 20:45:55 +0700

    edit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Pro from 'C:/Users/natty/front-week10/src/front/Product.js';
+import Product from 'C:/Users/natty/front-week10/src/front/Product.js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Pro />
+    <Product />
+  
   </React.StrictMode>
 );
 
