eefd36227023785a64ce562f6c94b0f05e2648e6
chollada punseenam <chollada.pu@ku.th>
2024-02-26 18:42:06 +0700

    import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Pro from 'C:/Users/natty/front-week10/src/front/Product.js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Pro />
   </React.StrictMode>
 );
 
